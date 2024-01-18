3f5861f5512e60c8ee19608e045f8a5203ad345b
uncenter <47499684+uncenter@users.noreply.github.com>
2024-01-17 23:32:49 -0500

    chore: remove eslint-plugin-prettier, update prettier & config

--- scripts/format-lang.js
+++ scripts/format-lang.js
@@ -1,7 +1,6 @@
 const fs = require('fs-extra');
 const path = require('path');
 const del = require('del');
-const prettier = require('prettier');
 
 const src = path.resolve(__dirname, '../src/lang');
 const dest = path.resolve(__dirname, '../build/messages');
@@ -14,21 +13,19 @@ This script takes the files from the `lang` folder and formats them into
 the format that format-js expects.
  */
 async function run() {
-  await fs.ensureDir(dest);
+	await fs.ensureDir(dest);
 
-  files.forEach(file => {
-    const lang = require(`../src/lang/${file}`);
-    const keys = Object.keys(lang).sort();
+	files.forEach(file => {
+		const lang = require(`../src/lang/${file}`);
+		const keys = Object.keys(lang).sort();
 
-    const formatted = keys.reduce((obj, key) => {
-      obj[key] = { defaultMessage: lang[key] };
-      return obj;
-    }, {});
+		const formatted = keys.reduce((obj, key) => {
+			obj[key] = { defaultMessage: lang[key] };
+			return obj;
+		}, {});
 
-    const json = prettier.format(JSON.stringify(formatted), { parser: 'json' });
-
-    fs.writeFileSync(path.resolve(dest, file), json);
-  });
+		fs.writeFileSync(path.resolve(dest, file), JSON.stringify(formatted));
+	});
 }
 
 run();
